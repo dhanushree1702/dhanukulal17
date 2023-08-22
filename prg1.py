575f8e9c747cd92fa5cbfbd4639bbb60a9ffb5f1
Dhanushree <dhanushree1705@gmail.com0>
2023-08-22 20:17:33 +0530

    Program1

---
+++ prg1.py
@@ -0,0 +1,8 @@
+#Program1
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6]])
+print("Array is of type: ",type(arr))
+print("No. of dimensions in array: ",arr.ndim)
+print("Shape of array: ",arr.shape)
+print("Size of array: ",arr.size)
+print("Array stores elements of type: ",arr.dtype)
